558282e236152ba99c607f2b01c715059357a02e
Nalanda-KF <nalandakippertflegler@gmail.com>
2024-02-25 17:49:09 -0400

    Atividade 01

---
+++ atividade01/lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Atividade_1());
+}
+
+class Atividade_1 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 84, 142, 229),
+            title: const Text("Cidades de Rondônia",
+                style: TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 213, 228, 239))),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              Container(
+                child: Image.asset('imagens/ji_Parana.jpg'),
+              ),
+              Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.only(left: 15, top: 15, right: 15),
+                  child: const Row(
+                    children: [
+                      Text('Brasil',
+                          style: TextStyle(
+                              fontSize: 25, 
+                              fontWeight: FontWeight.bold)
+                      ),
+                      Spacer(), // Ocupa o espaço restante na linha
+                      Icon(Icons.star,
+                          color: Color.fromARGB(255, 46, 106, 234)),
+                      Text("3.500",
+                          style: TextStyle(
+                            fontSize: 15,
+                          )),
+                    ],
+                  )),
+              Container(
+                  padding: const EdgeInsets.only(left: 15),
+                  alignment: Alignment.topLeft,
+                  child: const Column(
+                    children: [
+                      Text("Ji-Paraná, Rondônia",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromARGB(255, 73, 96, 114))),
+                    ],
+                  )),
+              Row(// para colocar os container em linha
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // alinha os conteiner com o mesmo espaçamento
+                children: [
+                  Container(
+                      padding: const EdgeInsets.only(top: 60),
+                      child: const Column(
+                        children: [
+                          Icon(Icons.phone,
+                              color: Color.fromARGB(255, 46, 106, 234)),
+                          Text("Ligar",
+                              style: TextStyle(
+                                fontSize: 15,
+                              )),
+                        ],
+                      )
+                  ),
+                  Container(
+                      padding: const EdgeInsets.only(top: 60),
+                      child: const Column(
+                        children: [
+                          Icon(Icons.map,
+                              color: Color.fromARGB(255, 46, 106, 234)),
+                          Text("Endereço",
+                              style: TextStyle(
+                                fontSize: 15,
+                              )),
+                        ],
+                      )
+                  ),
+                  Container(
+                      padding: const EdgeInsets.only(top: 60),
+                      child: const Column(
+                        children: [
+                          Icon(Icons.share,
+                              color: Color.fromARGB(255, 46, 106, 234)),
+                          Text("Compartilhar",
+                              style: TextStyle(
+                                fontSize: 15,
+                              )),
+                        ],
+                      )
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 60 , left: 10, right: 10),
+                child: const Row(
+                  children: [
+                    Expanded(
+                      // Faz com que o texto ocupe todo o espaço disponível
+                      child: Text(
+                        'Ji-Paraná é um município brasileiro do estado de Rondônia. Sua população, conforme estimativas do IBGE de 2021, era de 131.026 habitantes, sendo o segundo mais populoso do estado e o décimo sexto mais populoso da Região Norte do Brasil, a 226ª mais populosa do Brasil e a 113ª mais populosa cidade do interior brasileiro.',
+                        style: TextStyle(
+                            fontSize: 15, ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ], //children
+          )
+      ),
+    );
+  }
+}
